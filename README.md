182b617563d804891222667481392de9b6963a43
Seán C McCord <ulexus@gmail.com>
2016-10-25 00:29:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# snomcfg
+
+`snomcfg` is a simple webserver which serves SNOM 3xx configuration files.
+
+All files are expected to be created with one setting per line.  Each setting is
+a key-value pair separated by a `:`.  Comments may be used be beginning any line
+with `#`.
+
+## `/config`
+
+It is expected that the phone will supply its MAC address with the `?mac={mac}`
+suffix.  Thus a complete URL may look like:
+
+```
+  http://phone.mydomain.com:8080/config?mac={mac}
+```
+
+For any given phone, configuration variables are read from the following list of
+files, in order.  If any given setting exists in a later file, it will override any
+previous value.
+
+  - `snom-passwd.cfg`
+  - `snomXXX.cfg` (where XXX is the phone's model number, such as 360)
+  - `snom-001122334455.cfg (where 001122334455 is the phone's MAC address)`
+
+## `/firmware`
+
+Firmware reads firmware settings from the file `snomXXX-firmware.cfg` where XXX
+is the phone's model number, such as 360.
+
+
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+)
+
+// ErrComment indicates that the line was ignored due to it being commented out
+var ErrComment = errors.New("Comment line")
+
+var dir string
+
+var phoneTypeRegex = regexp.MustCompile(`snom\d{3}`)
+
+func main() {
+	addr := flag.String("l", ":8080", "Listen address")
+	flag.StringVar(&dir, "d", "/etc/asterisk/snom", "Source directory for configuration")
+
+	flag.Parse()
+
+	// Set handlers
+	http.HandleFunc("/config", config)
+	http.HandleFunc("/firmware", firmware)
+
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func firmware(w http.ResponseWriter, r *http.Request) {
+	ptype := getPhoneType(r.UserAgent())
+	if ptype == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid phone type"))
+		return
+	}
+	f, err := os.Open(path.Join(dir, fmt.Sprintf("%s-firmware.cfg", ptype)))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Failed to find firmware config"))
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
+	return
+}
+
+func config(w http.ResponseWriter, r *http.Request) {
+	cfg := make(map[string]string)
+
+	files := []string{"snom-passwd.cfg"}
+
+	// Find the phone type
+	ptype := getPhoneType(r.UserAgent())
+	if ptype != "" {
+		log.Println("Matched phone type", ptype)
+		files = append(files, ptype+".cfg")
+	}
+
+	// Find the MAC-specific config
+	mac := r.FormValue("mac")
+	if mac != "" {
+		log.Println("Matched MAC address", mac)
+		files = append(files, fmt.Sprintf("snom-%s.cfg", mac))
+	}
+
+	for _, fn := range files {
+		f, err := os.Open(path.Join(dir, fn))
+		if err != nil {
+			log.Println("Failed to open file", fn, err)
+		}
+		err = readLines(f, cfg)
+		if err != nil {
+			log.Println("Failed to read file", fn, err)
+		}
+		f.Close()
+	}
+
+	// Write the configuration to the ResponseWriter
+	for k, v := range cfg {
+		fmt.Fprintf(w, "%s: %s\n", k, v)
+	}
+	return
+}
+
+func getPhoneType(ua string) (ret string) {
+	return phoneTypeRegex.FindString(ua)
+}
+
+func parseLine(ln string) (key string, val string, err error) {
+	if strings.HasPrefix(ln, "#") {
+		err = ErrComment
+		return
+	}
+
+	// Split the line by the ':' character
+	pieces := strings.SplitN(ln, ":", 2)
+	if len(pieces) < 2 {
+		err = fmt.Errorf("Failed to parse line: %s", ln)
+		return
+	}
+
+	return strings.TrimSpace(pieces[0]), strings.TrimSpace(pieces[1]), nil
+}
+
+func readLines(in io.Reader, out map[string]string) error {
+	scanner := bufio.NewScanner(in)
+	for scanner.Scan() {
+		k, v, err := parseLine(scanner.Text())
+		if err == nil {
+			out[k] = v
+			continue
+		}
+		if err != ErrComment {
+			return err
+		}
+	}
+	return nil
+}
